56d09699a9ce0110ba3b546616474561dcdccd8f
Galit Weinfeld <105133862+Galitie@users.noreply.github.com>
2023-06-04 18:30:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 console.log("This code is linked to the html")
 
-let input = document.querySelector('input').value
+let input = document.querySelector('input')
 document.querySelector('button').addEventListener('click', test)
 
 function test(){
+  let userInputValue = input.value
   console.log("You pressed the submit button!")
-  console.log(input)
-
+  console.log(userInputValue)
+  
 }
