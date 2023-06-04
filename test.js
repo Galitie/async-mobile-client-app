9d420906b3a704a85e934ed1fe0bbdb96aa58588
Galit Weinfeld <105133862+Galitie@users.noreply.github.com>
2023-06-04 17:36:53 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("This is a test")
+function proceed () {
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', 'http://google.com');
+    form.style.display = 'hidden';
+    document.body.appendChild(form)
+    form.submit();
+}
