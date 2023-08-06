692f0ce7f329e5e05353688aa2e427618cbe491c
Galitie <galitdances@gmail.com>
2023-08-06 16:48:30 -0400

    Removed server status on phones for now

--- js/websocket.js
+++ js/websocket.js
@@ -1,7 +1,7 @@
 let canVibrate = false;
 
-let playerServerStatus = document.querySelector("h4");
-let hostServerStatus = document.querySelector("h3");
+// let playerServerStatus = document.querySelector("h4");
+// let hostServerStatus = document.querySelector("h3");
 
 const websocketUrl =
   "wss://13z2e6ro4l.execute-api.us-west-2.amazonaws.com/prod";
@@ -10,7 +10,7 @@ const socket = new WebSocket(websocketUrl);
 // Connection opened event and check for host
 socket.onopen = function (event) {
   console.log("WebSocket connection established.");
-  playerServerStatus.innerText = "\u2705 Connected to Server";
+  //playerServerStatus.innerText = "\u2705 Connected to Server";
 
   const packet = {
     action: "messageHost",
@@ -29,14 +29,14 @@ socket.onmessage = function (event) {
   }
 
   if (message["message"] == "Internal server error") {
-    hostServerStatus.innerText = "\u2717 Disconnected from Host";
-    hostServerStatus.style.color = "red";
+    //hostServerStatus.innerText = "\u2717 Disconnected from Host";
+    //hostServerStatus.style.color = "red";
     notConnectedScreen();
   } else if (message["message"] == "refuseJoin") {
     gameAlreadyStartedScreen();
   } else {
-    hostServerStatus.innerText = "\u2705 Host sent message!";
-    hostServerStatus.style.color = "white";
+    //hostServerStatus.innerText = "\u2705 Host sent message!";
+    //hostServerStatus.style.color = "white";
     if (canVibrate) navigator.vibrate(200);
     console.log(JSON.stringify(message));
     promptScreen(
@@ -60,8 +60,8 @@ socket.onerror = function (error) {
 // Connection closed event
 socket.onclose = function (event) {
   console.log("WebSocket connection closed:", event.code, event.reason);
-  playerServerStatus.innerText = "\u2717 Disconnected from Server";
-  playerServerStatus.style.color = "red";
+  //playerServerStatus.innerText = "\u2717 Disconnected from Server";
+  //playerServerStatus.style.color = "red";
   notConnectedScreen();
 };
 
