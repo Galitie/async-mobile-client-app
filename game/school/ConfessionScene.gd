06e1539839bc277dafc3df7899e897bcede1ace4
Galitie <galitdances@gmail.com>
2023-08-18 15:33:51 -0400

    Added confession scene dialogue

--- game/school/ConfessionScene.gd
+++ game/school/ConfessionScene.gd
@@ -15,13 +15,29 @@ var confessionMusic = load("res://school/confessionMusic.mp3")
 @onready var sakuraPetals = $CanvasLayer/SakuraPetals
 @onready var camera = get_parent().get_node("Camera2D")
 
+# placeholder
 var villain_portrait = snake_portrait
 
 var messages = [
-	Message.new("VILLIAN", "T-Tyler-sama...", "", Message.SignalTiming.NONE, [], null, villain_portrait),
-	Message.new("test", "Hey wattup", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
-	Message.new("VILLIAN", "Do u love me", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
-	Message.new("test", "Nope", "final_battle", Message.SignalTiming.DISAPPEAR, [], tyler_portrait, villain_portrait),
+	Message.new("Tyler", "Ok...I'm finally in the courtyard...", "", Message.SignalTiming.NONE, [], tyler_portrait, null),
+	Message.new("Tyler", "It was so hard telling all those other people that I'm not interested in them!", "", Message.SignalTiming.NONE, [], tyler_portrait, null),
+	Message.new("Tyler", "They all took it really well, sugoi!!", "", Message.SignalTiming.NONE, [], tyler_portrait, null),
+	Message.new("Tyler", "Now it's time to talk to the final person...", "", Message.SignalTiming.NONE, [], tyler_portrait, null),
+	Message.new("Tyler", "I bet this will go really well!!!!", "", Message.SignalTiming.NONE, [], tyler_portrait, null),
+	Message.new("Secret Admirer", "T-Tyler-sama...you came...", "", Message.SignalTiming.NONE, [], null, villain_portrait),
+	Message.new("Secret Admirer", "I...I have felt this way for so long...", "", Message.SignalTiming.NONE, [], null, villain_portrait),
+	Message.new("Tyler", "Hey wattup, I saw ur note", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Secret Admirer", "Do u love me Tyler-sama?", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Tyler", "...", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Tyler", "......", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Tyler", ".........", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Tyler", "no.", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Secret Admirer", "...h-huh?", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Secret Admirer", "b-but...Tyler-sama said no to everyone else...I thought...", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Secret Admirer", "I thought I was the one for you?!", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("VILLIAN", "If I can't have you, NO ONE CAN!", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("Tyler", "!!!NANI?!!!", "", Message.SignalTiming.NONE, [], tyler_portrait, villain_portrait),
+	Message.new("VILLIAN", "Prepare to DIE!!", "final_battle", Message.SignalTiming.DISAPPEAR, [], tyler_portrait, villain_portrait),
 ]
 
 func init():
